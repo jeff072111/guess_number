3ca1bcf8d645e97833b6053222129f6eea64e2c5
Jeffrey <jeff072111@gmail.com>
2020-12-18 15:01:34 +0800

    basic version

---
+++ guess_number.py
@@ -0,0 +1,12 @@
+import random
+r = random.randint(1, 100)
+while True:
+	num = input('請猜一個數字: ')
+	num = int(num)
+	if r == num:
+		print('終於猜對了')
+		break
+	elif num > r:
+		print('需要更小一點')
+	elif num < r:
+		print('需要更大一點')
